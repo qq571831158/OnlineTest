0a3f01349382367a078300c5f0b2266c7539b511
cheng <571831158@qq.com>
2018-02-04 15:46:37 +0800

    change deploy

--- src/main/java/com/orange/onlinetest/OnlinetestApplication.java
+++ src/main/java/com/orange/onlinetest/OnlinetestApplication.java
@@ -2,9 +2,17 @@ package com.orange.onlinetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OnlinetestApplication {
+public class OnlinetestApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OnlinetestApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlinetestApplication.class, args);
