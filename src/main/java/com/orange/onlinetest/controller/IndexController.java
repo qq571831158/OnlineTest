9f225ff31b97c7cc02a44a1ee31ee8dc0563f583
Apple <792378316@qq.com>
2018-04-14 13:48:48 +0800

    test

--- src/main/java/com/orange/onlinetest/controller/IndexController.java
+++ src/main/java/com/orange/onlinetest/controller/IndexController.java
@@ -35,6 +35,8 @@ public class IndexController {
         List<ViewObject> vos = stucorService.getStuIndexDataModel(hostHolder.getStus().getId());
         ModelAndView mv = new ModelAndView("student/home");
         mv.addObject("vos",vos);
+
+        
         return mv;
     }
 }
